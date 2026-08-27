f452822dae97390aa91e561d2db25558f54af36a
thai-nam <tn.ngo.marie8@gmail.com>
2026-08-27 02:13:19 +0200

    update of the package

--- middleware.ts
+++ middleware.ts
@@ -1,6 +1,6 @@
 import { type NextRequest } from "next/server";
 import { updateSession } from "@/lib/supabase/middleware";
-export const runtime = "edge";
+
 export async function middleware(request: NextRequest) {
   return await updateSession(request);
 }
