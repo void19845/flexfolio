b3ff5a6517cd5755413a5b046f2a93ff62fa4098
thai-nam <tn.ngo.marie8@gmail.com>
2026-08-27 02:05:57 +0200

    update of the package

--- middleware.ts
+++ middleware.ts
@@ -1,9 +1,10 @@
 import { type NextRequest } from "next/server";
 import { updateSession } from "@/lib/supabase/middleware";
-
+export const runtime = "edge";
 export async function middleware(request: NextRequest) {
   return await updateSession(request);
 }
+
 
 export const config = {
   matcher: ["/admin/:path*"],
